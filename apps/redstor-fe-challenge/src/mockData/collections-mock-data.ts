b97a16b057f2f7a2e0f671681689a961173a397d
Felipe <duarte_felipe022@hotmail.com>
2025-02-08 15:16:50 +0000

    fix nx structure

---
+++ apps/redstor-fe-challenge/src/mockData/collections-mock-data.ts
@@ -0,0 +1,136 @@
+import { ICollection } from '@app/interfaces';
+
+export const COLLECTIONS_MOCK_DATA: ICollection[] = [
+  {
+    id: 1,
+    title: 'Collection 1',
+    published_at: new Date(2025, 2, 8),
+    cover_photo: {
+      id: '1',
+      width: 400,
+      height: 400,
+      color: 'string',
+      description: 'string',
+      alt_description: 'string',
+      urls: {
+        raw: 'string',
+        full: 'string',
+        regular: 'string',
+        small: 'string',
+        thumb: 'string',
+        small_s3: 'string'
+      },
+      links: {
+        self: 'string',
+        html: 'string',
+        download: 'string',
+        download_location: 'string'
+      },
+      user: {
+        id: 'string',
+        username: 'string',
+        name: 'string',
+        first_name: 'string',
+        last_name: 'string',
+        profile_image: {
+          large: 'string',
+          medium: 'string',
+          small: 'string'
+        },
+        portfolio_url: 'string',
+        location: 'string'
+      },
+      likes: 23,
+      views: 123123
+    },
+    total_photos: 3
+  },
+  {
+    id: 2,
+    title: 'Collection 2',
+    published_at: new Date(2025, 2, 8),
+    cover_photo: {
+      id: '1',
+      width: 400,
+      height: 400,
+      color: 'string',
+      description: 'string',
+      alt_description: 'string',
+      urls: {
+        raw: 'string',
+        full: 'string',
+        regular: 'string',
+        small: 'string',
+        thumb: 'string',
+        small_s3: 'string'
+      },
+      links: {
+        self: 'string',
+        html: 'string',
+        download: 'string',
+        download_location: 'string'
+      },
+      user: {
+        id: 'string',
+        username: 'string',
+        name: 'string',
+        first_name: 'string',
+        last_name: 'string',
+        profile_image: {
+          large: 'string',
+          medium: 'string',
+          small: 'string'
+        },
+        portfolio_url: 'string',
+        location: 'string'
+      },
+      likes: 23,
+      views: 123123
+    },
+    total_photos: 3
+  },
+  {
+    id: 3,
+    title: 'Collection 3',
+    published_at: new Date(2025, 2, 8),
+    cover_photo: {
+      id: '1',
+      width: 400,
+      height: 400,
+      color: 'string',
+      description: 'string',
+      alt_description: 'string',
+      urls: {
+        raw: 'string',
+        full: 'string',
+        regular: 'string',
+        small: 'string',
+        thumb: 'string',
+        small_s3: 'string'
+      },
+      links: {
+        self: 'string',
+        html: 'string',
+        download: 'string',
+        download_location: 'string'
+      },
+      user: {
+        id: 'string',
+        username: 'string',
+        name: 'string',
+        first_name: 'string',
+        last_name: 'string',
+        profile_image: {
+          large: 'string',
+          medium: 'string',
+          small: 'string'
+        },
+        portfolio_url: 'string',
+        location: 'string'
+      },
+      likes: 23,
+      views: 123123
+    },
+    total_photos: 3
+  }
+];
---
+++ apps/redstor-fe-challenge/src/mockData/photo-mock-data.ts
@@ -0,0 +1,118 @@
+import { IPhoto } from '@app/interfaces';
+
+export const PHOTO_MOCK_DATA: IPhoto[] = [
+  {
+    id: '1',
+    width: 400,
+    height: 400,
+    color: 'string',
+    description: 'string',
+    alt_description: 'string',
+    urls: {
+      raw: 'string',
+      full: 'string',
+      regular: 'string',
+      small: 'string',
+      thumb: 'string',
+      small_s3: 'string'
+    },
+    links: {
+      self: 'string',
+      html: 'string',
+      download: 'string',
+      download_location: 'string'
+    },
+    user: {
+      id: 'string',
+      username: 'string',
+      name: 'string',
+      first_name: 'string',
+      last_name: 'string',
+      profile_image: {
+        large: 'string',
+        medium: 'string',
+        small: 'string'
+      },
+      portfolio_url: 'string',
+      location: 'string'
+    },
+    likes: 842,
+    views: 12754
+  },
+  {
+    id: '2',
+    width: 400,
+    height: 400,
+    color: 'string',
+    description: 'string',
+    alt_description: 'string',
+    urls: {
+      raw: 'string',
+      full: 'string',
+      regular: 'string',
+      small: 'string',
+      thumb: 'string',
+      small_s3: 'string'
+    },
+    links: {
+      self: 'string',
+      html: 'string',
+      download: 'string',
+      download_location: 'string'
+    },
+    user: {
+      id: 'string',
+      username: 'string',
+      name: 'string',
+      first_name: 'string',
+      last_name: 'string',
+      profile_image: {
+        large: 'string',
+        medium: 'string',
+        small: 'string'
+      },
+      portfolio_url: 'string',
+      location: 'string'
+    },
+    likes: 842,
+    views: 12754
+  },
+  {
+    id: '3',
+    width: 400,
+    height: 400,
+    color: 'string',
+    description: 'string',
+    alt_description: 'string',
+    urls: {
+      raw: 'string',
+      full: 'string',
+      regular: 'string',
+      small: 'string',
+      thumb: 'string',
+      small_s3: 'string'
+    },
+    links: {
+      self: 'string',
+      html: 'string',
+      download: 'string',
+      download_location: 'string'
+    },
+    user: {
+      id: 'string',
+      username: 'string',
+      name: 'string',
+      first_name: 'string',
+      last_name: 'string',
+      profile_image: {
+        large: 'string',
+        medium: 'string',
+        small: 'string'
+      },
+      portfolio_url: 'string',
+      location: 'string'
+    },
+    likes: 842,
+    views: 12754
+  }
+];
